556502cd90ff0c5000576dfa523a391c1b1831a2
CoreyJB247 <coreybruce1@protonmail.com>
2026-04-03 10:43:31 +0100

    Add files via upload
    Signed-off-by: CoreyJB247 <coreybruce1@protonmail.com>

--- data/items.lua
+++ data/items.lua
@@ -1,204 +1,551 @@
 return {
-	['bandage'] = {
-		label = 'Bandage',
-		weight = 115,
-		client = {
-			anim = { dict = 'missheistdockssetup1clipboard@idle_a', clip = 'idle_a', flag = 49 },
-			prop = { model = `prop_rolled_sock_02`, pos = vec3(-0.14, -0.14, -0.08), rot = vec3(-50.0, -50.0, 0.0) },
-			disable = { move = true, car = true, combat = true },
-			usetime = 2500,
-		}
-	},
-
-	['black_money'] = {
-		label = 'Dirty Money',
-	},
-
-	['burger'] = {
-		label = 'Burger',
-		weight = 220,
-		client = {
-			status = { hunger = 468000 },
-			anim = 'eating',
-			prop = 'burger',
-			usetime = 2500,
-			notification = 'You ate a delicious burger'
-		},
-	},
-
-	['sprunk'] = {
-		label = 'Sprunk',
-		weight = 350,
-		client = {
-			status = { thirst = 468000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_ld_can_01`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
-			usetime = 2500,
-			notification = 'You quenched your thirst with a sprunk'
-		}
-	},
-
-	['parachute'] = {
-		label = 'Parachute',
-		weight = 8000,
-		stack = false,
-		client = {
-			anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
-			usetime = 1500
-		}
-	},
-
-	['garbage'] = {
-		label = 'Garbage',
-	},
-
-	['paperbag'] = {
-		label = 'Paper Bag',
-		weight = 1,
-		stack = false,
-		close = false,
-		consume = 0
-	},
-
-	['identification'] = {
-		label = 'Identification',
-		client = {
-			image = 'card_id.png'
-		}
-	},
-
-	['panties'] = {
-		label = 'Knickers',
-		weight = 10,
-		consume = 0,
-		client = {
-			status = { thirst = -100000, stress = -25000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_cs_panties_02`, pos = vec3(0.03, 0.0, 0.02), rot = vec3(0.0, -13.5, -1.5) },
-			usetime = 2500,
-		}
-	},
-
-	['phone'] = {
-		label = 'Phone',
-		weight = 190,
-		stack = false,
-		consume = 0,
-		client = {
-			add = function(total)
-				if total > 0 then
-					pcall(function() return exports.npwd:setPhoneDisabled(false) end)
-				end
-			end,
-
-			remove = function(total)
-				if total < 1 then
-					pcall(function() return exports.npwd:setPhoneDisabled(true) end)
-				end
-			end
-		}
-	},
-
-	['money'] = {
-		label = 'Money',
-	},
-
-	['mustard'] = {
-		label = 'Mustard',
-		weight = 500,
-		client = {
-			status = { hunger = 25000, thirst = 25000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_food_mustard`, pos = vec3(0.01, 0.0, -0.07), rot = vec3(1.0, 1.0, -1.5) },
-			usetime = 2500,
-			notification = 'You.. drank mustard'
-		}
-	},
-
-	['water'] = {
-		label = 'Water',
-		weight = 500,
-		client = {
-			status = { thirst = 468000 },
-			anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
-			prop = { model = `prop_ld_flow_bottle`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(0.0, 0.0, -1.5) },
-			usetime = 2500,
-			cancel = true,
-			notification = 'You drank some refreshing water'
-		}
-	},
-
-	['radio'] = {
-		label = 'Radio',
-		weight = 1000,
-		stack = false,
-		allowArmed = true
-	},
-
-	['armour'] = {
-		label = 'Bulletproof Vest',
-		weight = 3000,
-		stack = false,
-		client = {
-			anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
-			usetime = 3500
-		}
-	},
-
-	['clothing'] = {
-		label = 'Clothing',
-		consume = 0,
-	},
-
-	['mastercard'] = {
-		label = 'Fleeca Card',
-		stack = false,
-		weight = 10,
-		client = {
-			image = 'card_bank.png'
-		}
-	},
-
-	['scrapmetal'] = {
-		label = 'Scrap Metal',
-		weight = 80,
-	},
-
-	["keys"] = {
-    label = "Car keys",
-    weight = 50,
-    stack = false,
-    close = true,
-    allowArmed = true,
-    server = {
-        export = "ND_Core.keys"
+    ['bandage'] = {
+        label = 'Bandage',
+        weight = 115,
     },
-    buttons = {
-        {
-            label = "Trunk",
-            action = function(slot)
-                exports["ND_Core"]:keyControl("trunk", slot)
-            end
+
+    ['burger'] = {
+        label = 'Burger',
+        weight = 220,
+        client = {
+            status = { hunger = 468000 },
+            anim = 'eating',
+            prop = 'burger',
+            usetime = 2500,
+            notification = 'You ate a delicious burger'
         },
-        {
-            label = "Disable key",
-            action = function(slot)
-                exports["ND_Core"]:keyControl("disable", slot)
+    },
+
+    ['sprunk'] = {
+        label = 'Sprunk',
+        weight = 350,
+        client = {
+            status = { thirst = 468000 },
+            anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+            prop = { model = `prop_ld_can_01`, pos = vec3(0.01, 0.01, 0.06), rot = vec3(5.0, 5.0, -180.5) },
+            usetime = 2500,
+            notification = 'You quenched your thirst with a sprunk'
+        }
+    },
+
+    ['parachute'] = {
+        label = 'Parachute',
+        weight = 8000,
+        stack = false,
+        client = {
+            anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
+            usetime = 1500
+        }
+    },
+
+    ['garbage'] = {
+        label = 'Garbage',
+    },
+
+    ['paperbag'] = {
+        label = 'Paper Bag',
+        weight = 1,
+        stack = false,
+        close = false,
+        consume = 0
+    },
+
+    ['panties'] = {
+        label = 'Knickers',
+        weight = 10,
+        consume = 0,
+        client = {
+            status = { thirst = -100000, stress = -25000 },
+            anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+            prop = { model = `prop_cs_panties_02`, pos = vec3(0.03, 0.0, 0.02), rot = vec3(0.0, -13.5, -1.5) },
+            usetime = 2500,
+        }
+    },
+
+    ['lockpick'] = {
+        label = 'Lockpick',
+        weight = 160,
+    },
+
+    ['phone'] = {
+        label = 'Phone',
+        weight = 190,
+        stack = false,
+        consume = 0,
+        client = {
+            add = function(total)
+                if total > 0 then
+                    pcall(function() return exports.npwd:setPhoneDisabled(false) end)
+                end
+            end,
+
+            remove = function(total)
+                if total < 1 then
+                    pcall(function() return exports.npwd:setPhoneDisabled(true) end)
+                end
             end
         }
-    }
-},
-["lockpick"] = {
-    label = "Lockpick",
-    weight = 160,
-    consume = 1,
-    stack = true,
-    close = true,
-    client = {
-        export = "ND_Core.lockpick"
-    }
-},
-["shield"] = {
+    },
+
+    ['mustard'] = {
+        label = 'Mustard',
+        weight = 500,
+        client = {
+            status = { hunger = 25000, thirst = 25000 },
+            anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+            prop = { model = `prop_food_mustard`, pos = vec3(0.01, 0.0, -0.07), rot = vec3(1.0, 1.0, -1.5) },
+            usetime = 2500,
+            notification = 'You... drank mustard'
+        }
+    },
+
+    ['water'] = {
+        label = 'Water',
+        weight = 500,
+        client = {
+            status = { thirst = 468000 },
+            anim = { dict = 'mp_player_intdrink', clip = 'loop_bottle' },
+            prop = { model = `prop_ld_flow_bottle`, pos = vec3(0.03, 0.03, 0.02), rot = vec3(0.0, 0.0, -1.5) },
+            usetime = 2500,
+            cancel = true,
+            notification = 'You drank some refreshing water'
+        }
+    },
+
+    ['armour'] = {
+        label = 'Bulletproof Vest',
+        weight = 3000,
+        stack = false,
+        client = {
+            anim = { dict = 'clothingshirt', clip = 'try_shirt_positive_d' },
+            usetime = 3500
+        }
+    },
+
+    ['clothing'] = {
+        label = 'Clothing',
+        consume = 0,
+    },
+
+    ['money'] = {
+        label = 'Money',
+    },
+
+    ['black_money'] = {
+        label = 'Dirty Money',
+    },
+
+    ['id_card'] = {
+        label = 'Identification Card',
+    },
+
+    ['driver_license'] = {
+        label = 'Drivers License',
+    },
+
+    ['weaponlicense'] = {
+        label = 'Weapon License',
+    },
+
+    ['lawyerpass'] = {
+        label = 'Lawyer Pass',
+    },
+
+    ['radio'] = {
+        label = 'Radio',
+        weight = 1000,
+        allowArmed = true,
+        consume = 0,
+        client = {
+            event = 'mm_radio:client:use'
+        }
+    },
+
+    ['jammer'] = {
+        label = 'Radio Jammer',
+        weight = 10000,
+        allowArmed = true,
+        client = {
+            event = 'mm_radio:client:usejammer'
+        }
+    },
+
+    ['radiocell'] = {
+        label = 'AAA Cells',
+        weight = 1000,
+        stack = true,
+        allowArmed = true,
+        client = {
+            event = 'mm_radio:client:recharge'
+        }
+    },
+
+    ['advancedlockpick'] = {
+        label = 'Advanced Lockpick',
+        weight = 500,
+    },
+
+    ['screwdriverset'] = {
+        label = 'Screwdriver Set',
+        weight = 500,
+    },
+
+    ['electronickit'] = {
+        label = 'Electronic Kit',
+        weight = 500,
+    },
+    
+    ['diamond_ring'] = {
+        label = 'Diamond',
+        weight = 1500,
+    },
+
+    ['rolex'] = {
+        label = 'Golden Watch',
+        weight = 1500,
+    },
+
+    ['goldbar'] = {
+        label = 'Gold Bar',
+        weight = 1500,
+    },
+
+    ['goldchain'] = {
+        label = 'Golden Chain',
+        weight = 1500,
+    },
+
+    ['crack_baggy'] = {
+        label = 'Crack Baggy',
+        weight = 100,
+    },
+
+    ['cokebaggy'] = {
+        label = 'Bag of Coke',
+        weight = 100,
+    },
+
+    ['coke_brick'] = {
+        label = 'Coke Brick',
+        weight = 2000,
+    },
+
+    ['coke_small_brick'] = {
+        label = 'Coke Package',
+        weight = 1000,
+    },
+
+    ['xtcbaggy'] = {
+        label = 'Bag of Ecstasy',
+        weight = 100,
+    },
+
+    ['meth'] = {
+        label = 'Methamphetamine',
+        weight = 100,
+    },
+
+    ['oxy'] = {
+        label = 'Oxycodone',
+        weight = 100,
+    },
+
+    ['weed_ak47'] = {
+        label = 'AK47 2g',
+        weight = 200,
+    },
+
+    ['weed_ak47_seed'] = {
+        label = 'AK47 Seed',
+        weight = 1,
+    },
+
+    ['weed_skunk'] = {
+        label = 'Skunk 2g',
+        weight = 200,
+    },
+
+    ['weed_skunk_seed'] = {
+        label = 'Skunk Seed',
+        weight = 1,
+    },
+
+    ['weed_amnesia'] = {
+        label = 'Amnesia 2g',
+        weight = 200,
+    },
+
+    ['weed_amnesia_seed'] = {
+        label = 'Amnesia Seed',
+        weight = 1,
+    },
+
+    ['weed_og-kush'] = {
+        label = 'OGKush 2g',
+        weight = 200,
+    },
+
+    ['weed_og-kush_seed'] = {
+        label = 'OGKush Seed',
+        weight = 1,
+    },
+
+    ['weed_white-widow'] = {
+        label = 'OGKush 2g',
+        weight = 200,
+    },
+
+    ['weed_white-widow_seed'] = {
+        label = 'White Widow Seed',
+        weight = 1,
+    },
+
+    ['weed_purple-haze'] = {
+        label = 'Purple Haze 2g',
+        weight = 200,
+    },
+
+    ['weed_purple-haze_seed'] = {
+        label = 'Purple Haze Seed',
+        weight = 1,
+    },
+
+    ['weed_brick'] = {
+        label = 'Weed Brick',
+        weight = 2000,
+    },
+
+    ['weed_nutrition'] = {
+        label = 'Plant Fertilizer',
+        weight = 2000,
+    },
+
+    ['joint'] = {
+        label = 'Joint',
+        weight = 200,
+    },
+
+    ['rolling_paper'] = {
+        label = 'Rolling Paper',
+        weight = 0,
+    },
+
+    ['empty_weed_bag'] = {
+        label = 'Empty Weed Bag',
+        weight = 0,
+    },
+
+    ['firstaid'] = {
+        label = 'First Aid',
+        weight = 2500,
+    },
+
+    ['ifaks'] = {
+        label = 'Individual First Aid Kit',
+        weight = 2500,
+    },
+
+    ['painkillers'] = {
+        label = 'Painkillers',
+        weight = 400,
+    },
+
+    ['firework1'] = {
+        label = '2Brothers',
+        weight = 1000,
+    },
+
+    ['firework2'] = {
+        label = 'Poppelers',
+        weight = 1000,
+    },
+
+    ['firework3'] = {
+        label = 'WipeOut',
+        weight = 1000,
+    },
+
+    ['firework4'] = {
+        label = 'Weeping Willow',
+        weight = 1000,
+    },
+
+    ['steel'] = {
+        label = 'Steel',
+        weight = 100,
+    },
+
+    ['rubber'] = {
+        label = 'Rubber',
+        weight = 100,
+    },
+
+    ['metalscrap'] = {
+        label = 'Metal Scrap',
+        weight = 100,
+    },
+
+    ['iron'] = {
+        label = 'Iron',
+        weight = 100,
+    },
+
+    ['copper'] = {
+        label = 'Copper',
+        weight = 100,
+    },
+
+    ['aluminum'] = {
+        label = 'Aluminium',
+        weight = 100,
+    },
+
+    ['plastic'] = {
+        label = 'Plastic',
+        weight = 100,
+    },
+
+    ['glass'] = {
+        label = 'Glass',
+        weight = 100,
+    },
+
+    ['gatecrack'] = {
+        label = 'Gatecrack',
+        weight = 1000,
+    },
+
+    ['cryptostick'] = {
+        label = 'Crypto Stick',
+        weight = 100,
+    },
+
+    ['trojan_usb'] = {
+        label = 'Trojan USB',
+        weight = 100,
+    },
+
+    ['toaster'] = {
+        label = 'Toaster',
+        weight = 5000,
+    },
+
+    ['small_tv'] = {
+        label = 'Small TV',
+        weight = 100,
+    },
+
+    ['security_card_01'] = {
+        label = 'Security Card A',
+        weight = 100,
+    },
+
+    ['security_card_02'] = {
+        label = 'Security Card B',
+        weight = 100,
+    },
+
+    ['drill'] = {
+        label = 'Drill',
+        weight = 5000,
+    },
+
+    ['thermite'] = {
+        label = 'Thermite',
+        weight = 1000,
+    },
+
+    ['diving_gear'] = {
+        label = 'Diving Gear',
+        weight = 30000,
+    },
+
+    ['diving_fill'] = {
+        label = 'Diving Tube',
+        weight = 3000,
+    },
+
+    ['antipatharia_coral'] = {
+        label = 'Antipatharia',
+        weight = 1000,
+    },
+
+    ['dendrogyra_coral'] = {
+        label = 'Dendrogyra',
+        weight = 1000,
+    },
+
+    ['jerry_can'] = {
+        label = 'Jerrycan',
+        weight = 3000,
+    },
+
+    ['wine'] = {
+        label = 'Wine',
+        weight = 500,
+    },
+
+    ['grape'] = {
+        label = 'Grape',
+        weight = 10,
+    },
+
+    ['grapejuice'] = {
+        label = 'Grape Juice',
+        weight = 200,
+    },
+
+    ['coffee'] = {
+        label = 'Coffee',
+        weight = 200,
+    },
+
+    ['vodka'] = {
+        label = 'Vodka',
+        weight = 500,
+    },
+
+    ['whiskey'] = {
+        label = 'Whiskey',
+        weight = 200,
+    },
+
+    ['beer'] = {
+        label = 'Beer',
+        weight = 200,
+    },
+
+    ['sandwich'] = {
+        label = 'Sandwich',
+        weight = 200,
+    },
+
+    ['walking_stick'] = {
+        label = 'Walking Stick',
+        weight = 1000,
+    },
+
+    ['lighter'] = {
+        label = 'Lighter',
+        weight = 200,
+    },
+
+    ['binoculars'] = {
+        label = 'Binoculars',
+        weight = 800,
+    },
+
+    ['stickynote'] = {
+        label = 'Sticky Note',
+        weight = 0,
+    },
+
+    ['harness'] = {
+        label = 'Harness',
+        weight = 200,
+    },
+
+    ["shield"] = {
         label = "Police shield",
         weight = 8000,
         stack = false,
@@ -238,18 +585,6 @@ return {
             export = "ND_Police.ziptie"
         }
     },
-    ["tools"] = {
-	label = "Tools",
-        description = "Can be used to hotwire vehicles.",
-	weight = 800,
-	consume = 1,
-        stack = true,
-        close = true,
-		client = {
-            export = "ND_Core.hotwire",
-            event = "ND_Police:unziptie"
-		}
-	},
     ["handcuffkey"] = {
         label = "Handcuff key",
         weight = 10,
@@ -377,4 +712,223 @@ return {
 ["casino_chips"] = {
     label = "Chips"
 },
+
+['advanced_pickaxe'] = {
+		label = 'Advanced Pickaxe',
+		weight = 100,
+		client = {
+			image = 'advanced_pickaxe.png',
+			usetime = 2500,
+		},
+	},
+
+	['blackpowder'] = {
+		label = 'Black Powder',
+		weight = 100,
+		client = {
+			image = 'blackpowder.png',
+			usetime = 2500,
+		},
+	},
+
+	['coal'] = {
+		label = 'Coal',
+		weight = 100,
+		client = {
+			image = 'coal.png',
+			usetime = 2500,
+		},
+	},
+
+	['concrete'] = {
+		label = 'Concrete',
+		weight = 100,
+		client = {
+			image = 'concrete.png',
+			usetime = 2500,
+		},
+	},
+
+	['copper_ingot'] = {
+		label = 'Copper Ingot',
+		weight = 100,
+		client = {
+			image = 'copper_ingot.png',
+			usetime = 2500,
+		},
+	},
+
+	['copper_ore'] = {
+		label = 'Copper Ore',
+		weight = 100,
+		client = {
+			image = 'copper_ore.png',
+			usetime = 2500,
+		},
+	},
+
+	['diamond_ingot'] = {
+		label = 'Diamond Ingot',
+		weight = 100,
+		client = {
+			image = 'diamond_ingot.png',
+			usetime = 2500,
+		},
+	},
+
+	['diamond_ore'] = {
+		label = 'Diamond Ore',
+		weight = 100,
+		client = {
+			image = 'diamond_ore.png',
+			usetime = 2500,
+		},
+	},
+
+	['glass'] = {
+		label = 'Glass',
+		weight = 100,
+		client = {
+			image = 'glass.png',
+			usetime = 2500,
+		},
+	},
+
+	['glass_mold'] = {
+		label = 'Glass Mold',
+		weight = 100,
+		client = {
+			image = 'glass_mold.png',
+			usetime = 2500,
+		},
+	},
+
+	['goldingot'] = {
+		label = 'Gold Ingot',
+		weight = 100,
+		client = {
+			image = 'goldingot.png',
+			usetime = 2500,
+		},
+	},
+
+	['gold_ore'] = {
+		label = 'Gold Ore',
+		weight = 100,
+		client = {
+			image = 'gold_ore.png',
+			usetime = 2500,
+		},
+	},
+
+	['ingot_mold'] = {
+		label = 'Ingot Mold',
+		weight = 100,
+		client = {
+			image = 'ingot_mold.png',
+			usetime = 2500,
+		},
+	},
+
+	['iron_ingot'] = {
+		label = 'Iron Ingot',
+		weight = 100,
+		client = {
+			image = 'iron_ingot.png',
+			usetime = 2500,
+		},
+	},
+
+	['iron_ore'] = {
+		label = 'Iron Ore',
+		weight = 100,
+		client = {
+			image = 'iron_ore.png',
+			usetime = 2500,
+		},
+	},
+
+	['limestone'] = {
+		label = 'Limestone',
+		weight = 100,
+		client = {
+			image = 'limestone.png',
+			usetime = 2500,
+		},
+	},
+
+	['normal_pickaxe'] = {
+		label = 'Normal Pickaxe',
+		weight = 100,
+		client = {
+			image = 'normal_pickaxe.png',
+			usetime = 2500,
+		},
+	},
+
+	['professional_pickaxe'] = {
+		label = 'Professional Pickaxe',
+		weight = 100,
+		client = {
+			image = 'professional_pickaxe.png',
+			usetime = 2500,
+		},
+	},
+
+	['rock'] = {
+		label = 'Rock',
+		weight = 100,
+		client = {
+			image = 'rock.png',
+			usetime = 2500,
+		},
+	},
+
+	['sandstone'] = {
+		label = 'Sandstone',
+		weight = 100,
+		client = {
+			image = 'sandstone.png',
+			usetime = 2500,
+		},
+	},
+
+	['special_water'] = {
+		label = 'Special Water',
+		weight = 100,
+		client = {
+			image = 'special_water.png',
+			usetime = 2500,
+		},
+	},
+
+	['sticky_gel'] = {
+		label = 'Sticky Gel',
+		weight = 100,
+		client = {
+			image = 'sticky_gel.png',
+			usetime = 2500,
+		},
+	},
+
+	['sulfur'] = {
+		label = 'Sulfur',
+		weight = 100,
+		client = {
+			image = 'sulfur.png',
+			usetime = 2500,
+		},
+	},
+
+    ['rentalpapers'] = {
+    label = "Rental Papers",
+    weight = 10,
+    stack = false,
+    close = false,
+    description = "Rental Papers",
+    client = {
+        image = "rentalpapers.png",
+    },
+    },
+
 }
--- data/shops.lua
+++ data/shops.lua
@@ -19,6 +19,22 @@ return {
 			vec3(2679.25, 3280.12, 55.24),
 			vec3(2557.94, 382.05, 108.62),
 			vec3(373.55, 325.56, 103.56),
+			vec3(268.88, -978.96, 30.6),
+			vec3(-47.84, -1758.28, 30.48),
+			vec3(292.12, -1278.52, 30.68),
+			vec3(1164.28, -323.8, 70.24),
+			vec3(-576.04, -1015.0, 23.44),
+			vec3(-706.64, -914.6, 20.24),
+			vec3(-692.28, -858.8, 24.84),
+			vec3(-2071.64, -339.44, 14.52),
+			vec3(192.16, -23.76, 71.12),
+			vec3(533.84, -153.68, 58.24),
+			vec3(1199.8, 2649.16, 39.04),
+			vec3(2004.92, 3783.8, 33.4),
+			vec3(-2539.92, 2314.0, 34.44),
+			vec3(-675.52, 5835.12, 18.32),
+			vec3(160.84, 6640.88, 32.92),
+			vec3(377.88, -1786.52, 30.52),
 		}, targets = {
 			{ loc = vec3(25.06, -1347.32, 29.5), length = 0.7, width = 0.5, heading = 0.0, minZ = 29.5, maxZ = 29.9, distance = 1.5 },
 			{ loc = vec3(-3039.18, 585.13, 7.91), length = 0.6, width = 0.5, heading = 15.0, minZ = 7.91, maxZ = 8.31, distance = 1.5 },
@@ -30,6 +46,46 @@ return {
 			{ loc = vec3(2678.29, 3279.94, 55.24), length = 0.6, width = 0.5, heading = 330.0, minZ = 55.24, maxZ = 55.64, distance = 1.5 },
 			{ loc = vec3(2557.19, 381.4, 108.62), length = 0.6, width = 0.5, heading = 0.0, minZ = 108.62, maxZ = 109.02, distance = 1.5 },
 			{ loc = vec3(373.13, 326.29, 103.57), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(268.88, -978.96, 29.6), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-47.84, -1758.28, 29.38), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(292.12, -1278.52, 29.58), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(1164.28, -323.8, 69.14), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-576.04, -1015.0, 22.24), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-706.64, -914.6, 18.94), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-692.28, -858.8, 23.54), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-2071.64, -339.44, 13.52), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(192.16, -23.76, 70.12), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(533.84, -153.68, 57.24), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(1199.8, 2649.16, 38.04), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(2004.92, 3783.8, 32.4), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-2539.92, 2314.0, 33.44), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(-675.52, 5835.12, 17.32), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(160.84, 6640.88, 31.92), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+			{ loc = vec3(377.88, -1786.52, 29.41), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5 },
+		}
+	},
+
+	MegaMall = {
+		name = 'Mega Mall',
+		blip = {
+			id = 59, colour = 30, scale = 0.8
+		}, inventory = {
+			{ name = 'burger', price = 1 },
+			{ name = 'water', price = 2 },
+			{ name = 'sprunk', price = 1 },
+			{ name = 'bandage', price = 15 },
+
+			{ name = 'binoculars', price = 30 },
+			{ name = 'lockpick', price = 10 },
+			{ name = 'phone', price = 400 },
+			{ name = 'radio', price = 100 },
+			{ name = 'WEAPON_WRENCH', price = 25 },
+			{ name = 'WEAPON_FLASHLIGHT', price = 20 },
+			{ name = 'WEAPON_CROWBAR', price = 15 },
+		}, locations = {
+			vec3(53.4, -1748.48, 30.52),
+		}, targets = {
+			{ loc = vec3(53.48, -1748.36, 29.52), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 2.5 },
 		}
 	},
 
@@ -48,14 +104,18 @@ return {
 			vec3(-2968.243, 390.910, 15.043),
 			vec3(1166.024, 2708.930, 38.157),
 			vec3(1392.562, 3604.684, 34.980),
-			vec3(-1393.409, -606.624, 30.319)
+			vec3(-1393.409, -606.624, 30.319),
+			vec3(208.28, -1768.8, 30.28),
+			vec3(-160.04, 6321.48, 32.68),
 		}, targets = {
 			{ loc = vec3(1134.9, -982.34, 46.41), length = 0.5, width = 0.5, heading = 96.0, minZ = 46.4, maxZ = 46.8, distance = 1.5 },
 			{ loc = vec3(-1222.33, -907.82, 12.43), length = 0.6, width = 0.5, heading = 32.7, minZ = 12.3, maxZ = 12.7, distance = 1.5 },
 			{ loc = vec3(-1486.67, -378.46, 40.26), length = 0.6, width = 0.5, heading = 133.77, minZ = 40.1, maxZ = 40.5, distance = 1.5 },
 			{ loc = vec3(-2967.0, 390.9, 15.14), length = 0.7, width = 0.5, heading = 85.23, minZ = 15.0, maxZ = 15.4, distance = 1.5 },
 			{ loc = vec3(1165.95, 2710.20, 38.26), length = 0.6, width = 0.5, heading = 178.84, minZ = 38.1, maxZ = 38.5, distance = 1.5 },
-			{ loc = vec3(1393.0, 3605.95, 35.11), length = 0.6, width = 0.6, heading = 200.0, minZ = 35.0, maxZ = 35.4, distance = 1.5 }
+			{ loc = vec3(1393.0, 3605.95, 35.11), length = 0.6, width = 0.6, heading = 200.0, minZ = 35.0, maxZ = 35.4, distance = 1.5 },
+			{ loc = vec3(208.28, -1768.8, 29.18), length = 0.6, width = 0.6, heading = 200.0, minZ = 35.0, maxZ = 35.4, distance = 1.5 },
+			{ loc = vec3(-160.04, 6321.48, 31.5), length = 0.6, width = 0.6, heading = 200.0, minZ = 35.0, maxZ = 35.4, distance = 1.5 },
 		}
 	},
 
@@ -64,9 +124,13 @@ return {
 		blip = {
 			id = 402, colour = 69, scale = 0.8
 		}, inventory = {
+			{ name = 'binoculars', price = 30 },
 			{ name = 'lockpick', price = 10 },
-			{ name = 'phone', price = 600 },
+			{ name = 'phone', price = 400 },
 			{ name = 'radio', price = 100 },
+			{ name = 'WEAPON_WRENCH', price = 25 },
+			{ name = 'WEAPON_FLASHLIGHT', price = 20 },
+			{ name = 'WEAPON_CROWBAR', price = 15 },
 		}, locations = {
 			vec3(2748.0, 3473.0, 55.67),
 			vec3(342.99, -1298.26, 32.51)
@@ -82,6 +146,7 @@ return {
 		}, inventory = {
 			{ name = 'ammo-9', price = 5, },
 			{ name = 'WEAPON_KNIFE', price = 200 },
+			{ name = 'WEAPON_FLASHLIGHT', price = 20 },
 			{ name = 'WEAPON_BAT', price = 100 },
 			{ name = 'WEAPON_PISTOL', price = 1000, metadata = { registered = true } }
 		}, locations = {
@@ -137,12 +202,12 @@ return {
 			{ name = 'WEAPON_DD14_B', price = 0 },
 			{ name = 'at_scope_ddm4_holo_b', price = 0 },
 			{ name = 'at_grip_bcm_b', price = 0 },
-			{ name = 'at_sfs14_b', price = 0 },
+			{ name = 'COMPONENT_AT_SCOPE_SMALL', price = 0 },
 		}, locations = {
-			vec3(479.88, -996.76, 31.68),
+			vec3(452.52, -980.08, 31.92),
 			vec3(-3165.68, 1116.72, 22.24),
 		}, targets = {
-			{ loc = vec3(479.88, -996.76, 30.68), length = 0.5, width = 3.0, heading = 270.0, minZ = 30.5, maxZ = 32.0, distance = 6 },
+			{ loc = vec3(453.28, -980.0, 30.68), length = 0.5, width = 3.0, heading = 270.0, minZ = 30.5, maxZ = 32.0, distance = 6 },
 			{ loc = vec3(-3165.68, 1116.72, 21.24), length = 0.5, width = 3.0, heading = 270.0, minZ = 30.5, maxZ = 32.0, distance = 6 },
 		}
 	},
